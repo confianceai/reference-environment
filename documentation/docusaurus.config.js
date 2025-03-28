4adbaa5ca8203c2d50b5df447dc3e46f5abcf466
yann.gaste <yann.gaste@irt-systemx.fr>
2025-03-28 14:10:45 +0100

    fix: update base url

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: "https://example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/reference-environment/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
